4a38714cd828ac47c3628589efeec13e3c94871b
Harikrishna266 <harikrishna266@gmail.com>
2016-10-19 20:07:37 +0530

    build fixes

--- src/app/audio-data.service.spec.ts
+++ src/app/audio-data.service.spec.ts
@@ -3,14 +3,14 @@
 import { addProviders, async, inject } from '@angular/core/testing';
 import { AudioDataService } from './audio-data.service';
 
-// describe('Service: AudioData', () => {
-//   beforeEach(() => {
-//     addProviders([AudioDataService]);
-//   });
+describe('Service: AudioData', () => {
+  beforeEach(() => {
+    addProviders([AudioDataService]);
+  });
 
-//   it('should ...',
-//     inject([AudioDataService],
-//       (service: AudioDataService) => {
-//         expect(service).toBeTruthy();
-//       }));
-// });
+  it('should ...',
+    inject([AudioDataService],
+      (service: AudioDataService) => {
+        expect(service).toBeTruthy();
+      }));
+});
