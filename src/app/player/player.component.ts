9f49eb9aeb45ce8eae23f16a2220e3a60954a5cb
Harikrishna266 <harikrishna266@gmail.com>
2016-10-24 16:47:30 +0530

    simple bar graph

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -20,6 +20,9 @@ export class PlayerComponent {
     public PlayableBuffer :any;
     public nowBuffering: any = [];
     public source: any;
+    public analyser:any;
+    public canvasctx:any;
+    public canvas: any;
     constructor() {
         this.context = new (window.AudioContext || window.webkitAudioContext)(); // define audio context
     }
@@ -56,13 +59,45 @@ export class PlayerComponent {
     }
 
     makeBufferSource() {
+        this.analyser = this.context.createAnalyser();
         this.source = this.context.createBufferSource();
+        this.source.connect(this.analyser);
         this.source.buffer = this.PlayableBuffer;
         this.source.connect(this.context.destination);
+        this.createCanvas();
+        this.frameLooper();
     }
 
     play() {
         this.source.start();
+    }
+    createCanvas() {
+        this.canvas = document.getElementById('analyser_render');
+        this.canvasctx = this.canvas.getContext('2d');    
+    }
+    frameLooper() {
+        // this.ngZone.runOutsideAngular(() => {
+             requestAnimationFrame(() => {
+                 console.log('in');
+                let fbc_array = new Uint8Array(this.analyser.frequencyBinCount);
+                this.analyser.getByteFrequencyData(fbc_array);
+                this.canvasctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Clear the canvas
+                this.canvasctx.fillStyle = '#00CCFF'; // Color of the bars
+                let bars = this.canvas.width;
+                let bar_x;
+                let bar_width;
+                let bar_height;
+
+
+                for (var i = 0; i < bars; i++) {
+                    bar_x = i * 3;
+                    bar_width = 2;
+                    bar_height = -(fbc_array[i] / 2);
+                    this.canvasctx.fillRect(bar_x, this.canvas.height, bar_width, bar_height);
+                }
+                this.frameLooper();
+             });
+        // });
     }
 
 
